dda3b6474cedc35f998a577155dee98a220c5bcf
sandesh <navaltajohan@gmail.com>
2022-05-16 12:19:56 +0545

    after removing errors

--- linkedList/src/main.rs
+++ linkedList/src/main.rs
@@ -1,67 +1,19 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-struct Node {
-    value: String,
-    next: Link,
-}
-
-impl Node {
-    fn new(value: String) -> Rc<RefCell<Node>> {
-        Rc::new(RefCell::new(Node {
-            value: value,
-            next: None,
-        }))
-    }
-}
-type Link = Option<Rc<RefCell<Node>>>;
-struct Transaction {
-    length: u8,
-    head: Link,
-    tail: Link,
-}
-impl Transaction {
-    fn empty() -> Transaction {
-        Transaction {
-            length: 0,
-            head: None,
-            tail: None,
-        }
-    }
-
-    fn append(&mut self, value: String) {
-        let new = Node::new(value);
-        match self.tail.take() {
-            Some(old) => old.borrow_mut().next = Some(new.clone()),
-            None => self.head = Some(new.clone()),
-        }
-        self.length += 1;
-        self.tail = Some(new)
-    }
-
-    fn pop(&mut self) -> String {
-        let head = self.head.clone();
-        match self.head.take() {
-            Some(value) => self.head = value.borrow_mut().next.take(),
-            None => {}
-        }
-        self.length -= 1;
-        Rc::try_unwrap(head.unwrap())
-            .ok()
-            .expect("something terrible papened")
-            .into_inner()
-            .value
-    }
-}
-fn main() {
+mod singlyLinked;
+use singlyLinked::Transaction;
+//use singlylinked::Transaction;
+fn main() {}
+#[cfg(test)]
+#[test]
+fn singlyLinkedList() {
     let mut transaction = Transaction::empty();
     transaction.append("this is a string".to_string());
 
     transaction.append("this is a another string".to_string());
-    transaction.append("this is a another string".to_string());
-    transaction.append("this is a another string".to_string());
+    transaction.append("this is a another string1".to_string());
+    transaction.append("this is a another string2".to_string());
     assert_eq!(transaction.length, 4);
-    println!("{}", transaction.pop());
-    println!("{}", transaction.pop());
-    println!("{}", transaction.pop());
+    assert_eq!(transaction.pop(), "this is a string".to_string());
+    assert_eq!(transaction.pop(), "this is a another string".to_string());
+    assert_eq!(transaction.pop(), "this is a another string1".to_string());
+    assert_eq!(transaction.pop(), "this is a another string2".to_string());
 }
---
+++ linkedList/src/singlyLinked.rs
@@ -0,0 +1,74 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+#[derive(Debug, Clone)]
+pub struct Node {
+    value: String,
+    next: Link,
+}
+
+impl Node {
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value: value,
+            next: None,
+        }))
+    }
+}
+type Link = Option<Rc<RefCell<Node>>>;
+pub struct Transaction {
+    pub length: u8,
+    head: Link,
+    pub tail: Link,
+}
+impl Transaction {
+    pub fn empty() -> Transaction {
+        Transaction {
+            length: 0,
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn append(&mut self, value: String) {
+        let new = Node::new(value);
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next = Some(new.clone()),
+            None => self.head = Some(new.clone()),
+        }
+        self.length += 1;
+        self.tail = Some(new)
+    }
+
+    pub fn pop(&mut self) -> String {
+        let head = self.head.clone();
+        match self.head.take() {
+            Some(value) => self.head = value.borrow_mut().next.take(),
+            None => {}
+        }
+        self.length -= 1;
+        println!(" head {:?}", &head);
+        head.unwrap().borrow_mut().value.clone()
+
+        /* Another way of implementation
+         * Rc::try_unwrap(head.unwrap())
+            .ok()
+            .expect("something terrible papened")
+            .into_inner()
+            .value
+
+            self.head.take().map(|head| {
+                if let Some(next) = head.borrow_mut().next.take() {
+                    self.head = Some(next);
+                } else {
+                    self.tail.take();
+                }
+                self.length -= 1;
+                Rc::try_unwrap(head)
+                    .ok()
+                    .expect("Something is terribly wrong")
+                    .into_inner()
+                    .value
+            })
+        */
+    }
+}
